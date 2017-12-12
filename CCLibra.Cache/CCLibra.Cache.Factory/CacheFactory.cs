9ab662d8d63b9043aae8704e88e6023318a63288
chuchao87826 <348249224@qq.com>
2017-12-12 18:04:47 +0800

    梳理框架

---
+++ CCLibra.Cache/CCLibra.Cache.Factory/CacheFactory.cs
@@ -0,0 +1,20 @@
+﻿using CCLibra.Cache.Base;
+using CCLibra.Cache.Redis;
+
+namespace CCLibra.Cache.Factory
+{
+    /// <summary>
+    /// 版 本 CCLibra 框架 
+    /// Copyright (c) chuchao
+    /// 创建人：力软-框架开发组
+    /// 日 期：2017.03.06
+    /// 描 述：定义缓存工厂类
+    /// </summary>
+    public class CacheFactory
+    {
+        public static ICache CaChe()
+        {
+            return new CacheByRedis();
+        }
+    }
+}
