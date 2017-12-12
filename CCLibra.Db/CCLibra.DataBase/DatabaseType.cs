a6e112cad044d1211041cce2d4c8c299b04578b2
chuchao87826 <348249224@qq.com>
2017-12-12 17:04:23 +0800

    梳理框架结构

---
+++ CCLibra.Db/CCLibra.DataBase/DatabaseType.cs
@@ -0,0 +1,34 @@
+﻿
+namespace CCLibra.DataBase
+{
+    /// <summary>
+    /// 版 本 CCLibra 框架
+    /// Copyright (c) chuchao
+    /// 创建人：褚超
+    /// 日 期：2017.12.12
+    /// 描 述：数据库类型枚举
+    /// </summary>
+    public enum DatabaseType
+    {
+        /// <summary>
+        /// 数据库类型：SqlServer
+        /// </summary>
+        SqlServer,
+        /// <summary>
+        /// 数据库类型：MySql
+        /// </summary>
+        MySql,
+        /// <summary>
+        /// 数据库类型：Oracle
+        /// </summary>
+        Oracle,
+        /// <summary>
+        /// 数据库类型：Access
+        /// </summary>
+        Access,
+        /// <summary>
+        /// 数据库类型：SQLite
+        /// </summary>
+        SQLite
+    }
+}
